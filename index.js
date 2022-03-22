b050b4c51b8c8b921a504f3992000922e696cb0e
yugmani <yugmani@yahoo.com>
2022-03-21 22:25:01 -0700

    sort by age of users object

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 
 // You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+let john = { name: 'John', age: 25 };
+let pete = { name: 'Pete', age: 30 };
+let mary = { name: 'Mary', age: 28 };
 
-let users = [ john, pete, mary ];
+let users = [john, pete, mary];
 
-let names = users.map(user=> user.name);
-console.log(names);   //["John", "Pete", "Mary"]
+let names = users.map((user) => user.name);
+console.log(names); //["John", "Pete", "Mary"]
 
 // Map to objects
 // **************************************
@@ -19,20 +19,19 @@ console.log(names);   //["John", "Pete", "Mary"]
 
 // Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
 
-let johnS = { name: "John", surname: "Smith", id: 1 };
-let peteH = { name: "Pete", surname: "Hunt", id: 2 };
-let maryK = { name: "Mary", surname: "Key", id: 3 };
+let johnS = { name: 'John', surname: 'Smith', id: 1 };
+let peteH = { name: 'Pete', surname: 'Hunt', id: 2 };
+let maryK = { name: 'Mary', surname: 'Key', id: 3 };
 
-let usersGroup = [ johnS, peteH, maryK ];
+let usersGroup = [johnS, peteH, maryK];
 
-let usersMapped = usersGroup.map(user=>(
-  { // note that we need additional brackets '()'.
-    fullName: `${user.name} ${user.surname}`,
-    id:user.id 
-  }
-));
+let usersMapped = usersGroup.map((user) => ({
+  // note that we need additional brackets '()'.
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id,
+}));
 
-console.log(usersMapped); 
+console.log(usersMapped);
 /* 
 usersMapped = [
   { fullName: "John Smith", id: 1 },
@@ -40,6 +39,18 @@ usersMapped = [
   { fullName: "Mary Key", id: 3 }
 ]
 */
-console.log(usersMapped[0].id);   //1
+console.log(usersMapped[0].id); //1
 console.log(usersMapped[0].fullName); //John Smith
+
+// Sort users by age
+// ****************************************
+
+// Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+const sortByAge = (arr) => {
+  return arr.sort((a, b) => a.age - b.age);
+};
+
+console.log(sortByAge(users));
+
 
